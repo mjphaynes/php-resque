973889e9323bdbbcbbb16f9fbd3cfcc6f6a6917b
Francis Lavoie <lavofr@gmail.com>
2017-01-09 14:48:05 -0500

    Change composer versions, use alias for helpers

--- src/Resque/Commands/Clear.php
+++ src/Resque/Commands/Clear.php
@@ -39,7 +39,7 @@ class Clear extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $helper = $this->getHelperSet()->get('question');
+        $helper = $this->getHelper('question');
 
         $question = new ConfirmationQuestion('Continuing will clear all php-resque data from Redis. Are you sure? ', false);
 
