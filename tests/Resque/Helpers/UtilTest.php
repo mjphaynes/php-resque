1f71e923a74edd8cbec5677c8af3e7888108717c
Dirk Gustke <dirk.gustke@vmpublishing.com>
2018-06-11 16:40:13 +0200

    Cleaner namespacing

--- tests/Resque/Helpers/UtilTest.php
+++ tests/Resque/Helpers/UtilTest.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Resque\Tests\Helpers\Utils;
+namespace Resque\Tests\Helpers;
 
 use Resque\Helpers\Util;
 use PHPUnit\Framework\TestCase;
