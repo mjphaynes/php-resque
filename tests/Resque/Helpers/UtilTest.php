e3922a3d7daad4f99afc5ecba9f20fdcca84bb39
Dirk Gustke <dirk.gustke@vmpublishing.com>
2018-06-11 12:40:04 +0200

    Typo by original author

--- tests/Resque/Helpers/UtilTest.php
+++ tests/Resque/Helpers/UtilTest.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Resque\Tests\Heplers\Utils;
+namespace Resque\Tests\Helpers\Utils;
 
 use Resque\Helpers\Util;
 use PHPUnit\Framework\TestCase;
